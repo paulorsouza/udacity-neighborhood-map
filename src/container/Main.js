c3d54497777cc6ef678f48952ae0e63241bd3579
Paulo Ricardo <paulor1809@gmail.com>
2018-08-08 01:36:53 +0000

    Create Marker component

--- src/container/Main.js
+++ src/container/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GoogleApi from '../hoc/GoogleApi';
 import Map from '../component/Map';
+import Marker from '../component/Marker';
 
 class Main extends Component {
   render() {
@@ -8,10 +9,13 @@ class Main extends Component {
       width: '100vw',
       height: '100vh'
     }
+    const pos = {lat: 37.759703, lng: -122.428093}
     return (
       <div style={style}>
-        <Map google={this.props.google}
-          />
+        <Map google={this.props.google}>
+          <Marker />
+          <Marker position={pos} />
+        </Map>
       </div>
     )
   }
