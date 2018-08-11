08941c212ebfd822db9224df38d887d71717ea5d
Paulo Ricardo <paulor1809@gmail.com>
2018-08-10 21:39:35 -0300

    Add icons to markers

--- src/container/Map.js
+++ src/container/Map.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Map from '../component/Map';
 import Marker from '../component/Marker';
+import icons from '../util/icons';
 
 export default class MapContainer extends Component {
   static propTypes = {
@@ -25,9 +26,85 @@ export default class MapContainer extends Component {
         lng={bombinhas.lng}
       >
         <Marker
-          name="Dolores park"
-          lat={-27.151063}
-          lng={-48.485371}
+          name="Pousada Ganesh"
+          lat={-27.1513326}
+          lng={-48.4854157}
+          icon={{
+            url: icons.bed,
+            scaledSize: new gmap.Size(38, 38)
+          }}
+        />
+        <Marker
+          name="Sushi Bar Ganesh"
+          lat={-27.1510912}
+          lng={-48.4852396}
+          icon={{
+            url: icons.sushi,
+            scaledSize: new gmap.Size(32, 32)
+          }}
+        />
+        <Marker
+          name="João Sorveteria"
+          lat={-27.1493531}
+          lng={-48.4871171}
+          icon={{
+            url: icons.iceCream,
+            scaledSize: new gmap.Size(32, 32)
+          }}
+        />
+        <Marker
+          name="Super Mercado Bombinhas"
+          lat={-27.1488612}
+          lng={-48.4836138}
+          icon={{
+            url: icons.cart,
+            scaledSize: new gmap.Size(32, 32)
+          }}
+        />
+        <Marker
+          name="Pousada Canto das Trilhas"
+          lat={-27.1448554}
+          lng={-48.5046959}
+          icon={{
+            url: icons.cabin,
+            scaledSize: new gmap.Size(40, 40)
+          }}
+        />
+        <Marker
+          name="Pousada Bomar Bombinhas"
+          lat={-27.1493531}
+          lng={-48.4871171}
+          icon={{
+            url: icons.hotel,
+            scaledSize: new gmap.Size(32, 32)
+          }}
+        />
+        <Marker
+          name="Olímpio"
+          lat={-27.1458787}
+          lng={-48.5014784}
+          icon={{
+            url: icons.food,
+            scaledSize: new gmap.Size(38, 38)
+          }}
+        />
+        <Marker
+          name="Apart Hotel"
+          lat={-27.1397874}
+          lng={-48.5101237}
+          icon={{
+            url: icons.hotel,
+            scaledSize: new gmap.Size(32, 32)
+          }}
+        />
+        <Marker
+          name="Armazém quatro ilhas"
+          lat={-27.151553}
+          lng={-48.486435}
+          icon={{
+            url: icons.cart,
+            scaledSize: new gmap.Size(32, 32)
+          }}
         />
       </Map>
     );
---
+++ src/util/icons.js
@@ -0,0 +1,17 @@
+import cabin from '../../assets/img/cabin.png';
+import bed from '../../assets/img/bed.png';
+import cart from '../../assets/img/cart.png';
+import food from '../../assets/img/food.png';
+import hotel from '../../assets/img/hotel.png';
+import iceCream from '../../assets/img/ice-cream.png';
+import sushi from '../../assets/img/sushi.png';
+
+export default {
+  cabin,
+  bed,
+  cart,
+  food,
+  hotel,
+  iceCream,
+  sushi
+};
